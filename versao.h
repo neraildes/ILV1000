d7216f71ff686e3ffece87f37b16c77e5708625d
neraildes <neraildes@gmail.com>
2021-05-23 05:22:44 -0300

    Todos os reles ciclando - Falta informacao para terminal e documentacao

--- versao.h
+++ versao.h
@@ -1,6 +1,6 @@
 // arquivo criado pelo script versionar.sh
-// neraildes - Fri, 21 May 2021 11:23:34 -0300
+// neraildes - Sun, 23 May 2021 05:19:59 -0300
 #ifndef VERSAO_H
 #define	VERSAO_H
-#define FVERSION "ATUAL 1.0.3_BETA_21-05-2021"
+#define FVERSION "ATUAL 1.0.4_BETA"
 #endif
