89db999dde97f7454246b75f1a0649cd5142a55e
neraildes <neraildes@gmail.com>
2021-05-24 09:31:22 -0300

    Colocar pow(x) macro

--- versao.h
+++ versao.h
@@ -1,6 +1,6 @@
 // arquivo criado pelo script versionar.sh
-// neraildes - Sun, 23 May 2021 05:19:59 -0300
+// neraildes - Mon, 24 May 2021 09:29:18 -0300
 #ifndef VERSAO_H
 #define	VERSAO_H
-#define FVERSION "ATUAL 1.0.4_BETA"
+#define FVERSION "ATUAL 1.0.5_BETA"
 #endif
