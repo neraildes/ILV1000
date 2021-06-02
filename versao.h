16e844df90b2ebe3d8eb006b61f359fd19b2c6c3
neraildes <neraildes@gmail.com>
2021-06-02 06:36:22 -0300

    Versão funcionando. Será implementado Bluetooth Terminal.

--- versao.h
+++ versao.h
@@ -1,6 +1,6 @@
 // arquivo criado pelo script versionar.sh
-// neraildes - Mon, 31 May 2021 18:03:07 -0300
+// neraildes - Wed, 2 Jun 2021 06:33:50 -0300
 #ifndef VERSAO_H
 #define	VERSAO_H
-#define FVERSION "ATUAL 1.0.10_BETA"
+#define FVERSION "ATUAL 1.0.11_BETA"
 #endif
