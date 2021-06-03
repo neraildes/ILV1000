8d1eca7488b1432f724b766e5f98c4aea751fd59
neraildes <neraildes@gmail.com>
2021-06-03 07:48:52 -0300

    Sem repique no rele condensador, sem bluethooth

--- versao.h
+++ versao.h
@@ -1,6 +1,6 @@
 // arquivo criado pelo script versionar.sh
-// neraildes - Wed, 2 Jun 2021 06:33:50 -0300
+// neraildes - Thu, 3 Jun 2021 07:46:25 -0300
 #ifndef VERSAO_H
 #define	VERSAO_H
-#define FVERSION "ATUAL 1.0.11_BETA"
+#define FVERSION "ATUAL 1.0.15_BETA"
 #endif
