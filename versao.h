412747018b4f85d4f15dcddc9e83b03a2a688e20
neraildes <neraildes@gmail.com>
2021-06-11 21:16:07 -0300

    primeiro commit desta versao

--- versao.h
+++ versao.h
@@ -1,6 +1,6 @@
 // arquivo criado pelo script versionar.sh
-// neraildes - Thu, 3 Jun 2021 11:23:25 -0300
+// neraildes - Fri, 11 Jun 2021 21:13:10 -0300
 #ifndef VERSAO_H
 #define	VERSAO_H
-#define FVERSION "ATUAL 1.0.16_BETA"
+#define FVERSION "ATUAL 1.0.19_BETA"
 #endif
