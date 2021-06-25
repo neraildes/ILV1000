0d31bfbec5a81124eb708a82d3f621dfb35ea95a
neraildes <neraildes@gmail.com>
2021-06-25 05:18:19 -0300

    Modificação nos códigos das teclas

--- versao.h
+++ versao.h
@@ -1,6 +1,6 @@
 // arquivo criado pelo script versionar.sh
-// neraildes - Mon, 14 Jun 2021 19:38:35 -0300
+// neraildes - Thu, 24 Jun 2021 09:04:47 -0300
 #ifndef VERSAO_H
 #define	VERSAO_H
-#define FVERSION "ATUAL 1.0.20_BETA"
+#define FVERSION "ATUAL 1.0.21_BETA"
 #endif
