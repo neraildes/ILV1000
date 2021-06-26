8a30224279606e20ba5cd547e7884833e74e14b5
neraildes <neraildes@gmail.com>
2021-06-26 11:48:07 -0300

    Primeiro commit da versao

--- versao.h
+++ versao.h
@@ -1,6 +1,6 @@
 // arquivo criado pelo script versionar.sh
-// neraildes - Thu, 24 Jun 2021 09:04:47 -0300
+// neraildes - Sat, 26 Jun 2021 11:46:02 -0300
 #ifndef VERSAO_H
 #define	VERSAO_H
-#define FVERSION "ATUAL 1.0.21_BETA"
+#define FVERSION "ATUAL 1.0.22_BETA"
 #endif
